4c43e0fff09c11471e0c51a5c5a457289197d056
DeepralShakya <shakyadeepral44@gmail.com>
2024-12-16 16:36:21 +0545

    added adsense

--- app/layout.js
+++ app/layout.js
@@ -29,6 +29,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6229661577531203"
+        crossorigin="anonymous"></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
